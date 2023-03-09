aeb70fd9457f0a6be2207aa8d144ab5f09d6b12c
sberens <sberensnyc@gmail.com>
2023-03-08 21:21:47 -0800

    write results to json

--- run_default_pipeline.py
+++ run_default_pipeline.py
@@ -1,17 +1,18 @@
-from pprint import pprint
-
+import paths
 from execution_pipeline.code_exectuors.python_executor import PythonExecutor
 from execution_pipeline.evaluators.stripped_exact_match import StrippedExactMatch
 from execution_pipeline.languages import SupportedLanguage
 from execution_pipeline.llm_executors.python_openai import PythonOpenAiExecutor
 from execution_pipeline.pipelines.default_pipeline import DefaultPipeline
 from execution_pipeline.task_runners.default_task_runner import DefaultTaskRunner
-
+from wrtier import write_test_results
 code_executor_mapping = {SupportedLanguage.PYTHON: PythonExecutor()}
 llm_executor_mapping = {SupportedLanguage.PYTHON: [PythonOpenAiExecutor()]}
 
 task_runner = DefaultTaskRunner(code_executor_mapping, llm_executor_mapping, StrippedExactMatch())
 
+print("Running pipeline...")
 results = DefaultPipeline(task_runner).run()
-
-pprint(results)
+print("Writing results...")
+write_test_results(results, paths.root_path.joinpath('results/result.json'))
+print("Done!")
---
+++ wrtier.py
@@ -0,0 +1,53 @@
+import json
+from pathlib import Path
+
+import paths
+from execution_pipeline.types import TestResult, SubtestResult
+
+
+def get_key_from_path(path: Path):
+    return path.relative_to(paths.tasks_path).as_posix()
+
+
+def get_json_subtest_result(subtest_result: SubtestResult):
+    return {
+        "language": subtest_result.language.value,
+        "input_text_key": get_key_from_path(subtest_result.input_path),
+        "code_file_str_key": get_key_from_path(subtest_result.code_path),
+        "llm_executor_name": subtest_result.llm_executor.name,
+        "predicted_output": subtest_result.predicted_output,
+        "code_output": subtest_result.code_output,
+        "correctly_predicted": subtest_result.correctly_predicted
+    }
+
+
+def get_json_test_results(test_results: list[TestResult]):
+    res_json = {
+        "input_texts": {},
+        "code_file_strs": {},
+        "task_infos": {},
+        "results": []
+    }
+
+    for input_path in paths.tasks_path.glob("*/inputs/*"):
+        res_json["input_texts"][get_key_from_path(input_path)] = input_path.read_text()
+
+    for code_path in paths.tasks_path.glob("*/code/*"):
+        res_json["code_file_strs"][get_key_from_path(code_path)] = code_path.read_text()
+
+    for test_result in test_results:
+        res_json["task_infos"][get_key_from_path(test_result.path)] = {
+            "name": test_result.path.stem,
+            "metadata": test_result.metadata
+        }
+        for subtest_result in test_result.subtest_results:
+            res_json["results"].append({
+                "task_info_key": get_key_from_path(test_result.path),
+                **get_json_subtest_result(subtest_result)
+            })
+    return res_json
+
+
+def write_test_results(test_results: list[TestResult], path: Path):
+    path.parent.mkdir(exist_ok=True, parents=True)
+    path.write_text(json.dumps(get_json_test_results(test_results)))
