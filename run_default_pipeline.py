74b3f951614d8774c80c9ec2b3b3a79d6dcbb52e
sberens <sberensnyc@gmail.com>
2023-03-17 13:11:06 -0700

    change cohere model to nightly

--- run_default_pipeline.py
+++ run_default_pipeline.py
@@ -15,7 +15,7 @@ from writer import write_test_results
 llm_infos = [
     [OpenAI(temperature=0), "openai", True],
     [OpenAI(temperature=0, model_name="text-davinci-002"), "openai", True],
-    [Cohere(model="command-xlarge-20221108", temperature=0), "cohere", False]
+    [Cohere(model="command-xlarge-nightly", temperature=0), "cohere", False]
 ]
 
 python_llm_executors = [
